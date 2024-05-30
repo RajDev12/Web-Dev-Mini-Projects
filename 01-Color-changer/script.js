775ed8edf0500a6d010e7589823bc87afafe6c07
RajDev12 <rjnath98@gmail.com>
2024-05-30 12:12:50 +0530

    eventListener on button

--- 01-Color-changer/script.js
+++ 01-Color-changer/script.js
@@ -1,1 +1,42 @@
- 
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+//using arrow function in event listener
+
+buttons.forEach((button) => {
+    console.log(button)
+    button.addEventListener('click', (e) => {
+        console.log(e)
+        console.log(e.target)
+        if (e.target.id === 'grey') {
+            body.style.backgroundColor = 'grey';
+        } else if (e.target.id === 'white') {
+            body.style.backgroundColor = 'white'
+        } else if (e.target.id === 'blue') {
+            body.style.backgroundColor = 'blue'
+        } else {
+            body.style.backgroundColor = 'yellow'
+        }
+
+    })
+})
+
+// buttons.forEach(function (button) {
+//     console.log(button)
+//     button.addEventListener('click', function (e) {
+//         console.log(e) //click on a button and check the console
+//         console.log(e.target)
+//         if (e.target.id === 'grey') {
+//             body.style.backgroundColor = 'grey';
+//         } else if (e.target.id === 'white') {
+//             body.style.backgroundColor = 'white'
+//         } else if (e.target.id === 'blue') {
+//             body.style.backgroundColor = 'blue'
+//         } else {
+//             body.style.backgroundColor = 'yellow'
+//         }
+
+//     })
+
+// })
+
